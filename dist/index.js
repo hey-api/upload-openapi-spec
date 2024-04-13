c4cbf16708227d32e133d3faa87e9c691883e2c2
Lubos <lmenus@lmen.us>
2024-04-13 21:16:09 +0100

    chore: write to database

--- dist/index.js
+++ dist/index.js
@@ -41642,17 +41642,29 @@ async function upload(pathToOpenApi) {
             throw new Error('OpenAPI path is invalid');
         }
         const fileBody = (0, node_fs_1.readFileSync)(pathToOpenApi);
-        const parts = pathToOpenApi.split('/');
+        const match = pathToOpenApi.match(/\.[0-9a-z]+$/i);
+        const extension = match ? match[0] : '';
         // TODO: replace with unique bucket name
-        const name = `@hey-api/upload-openapi-spec/${new Date().toISOString()}`;
-        const { error } = await supabase.storage
+        const fileName = new Date().toISOString() + extension;
+        const org = '@hey-api';
+        const project = 'upload-openapi-spec';
+        const filePath = `${org}/${project}/${fileName}`;
+        const uploadResponse = await supabase.storage
             .from('openapi-specs')
-            .upload(name, fileBody, {
+            .upload(filePath, fileBody, {
             cacheControl: '3600',
             upsert: false
         });
-        if (error) {
-            throw new Error(error.message);
+        if (uploadResponse.error) {
+            throw new Error(uploadResponse.error.message);
+        }
+        const insertResponse = await supabase.from('openapi-specs').insert({
+            org,
+            path: filePath,
+            project
+        });
+        if (insertResponse.error) {
+            throw new Error(insertResponse.error.message);
         }
         resolve();
     });
